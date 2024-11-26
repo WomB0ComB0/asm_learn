d085ba52426c5bddd8e0574db032d3aa4cb724e2
Mike Odnis <airwolf635@gmail.com>
2024-11-25 22:14:35 -0500

    feat: folder structure

--- go.sh
+++ go.sh
@@ -1,9 +1,10 @@
 cats=("hello_world" "jumps_calls_comparisons" "input" "math_stack" "subroutine_print_strings" "macros" "subroutine_print_integers" "cli_args" "file_perms" "files_writes" "files_reads" "nanosleep")
 
 for i in ${!cats[@]}; do
-    touch "learn-$((i+1)).s"
-    echo "section .data" > "learn-$((i+1)).s"
-    echo "section .text" >> "learn-$((i+1)).s"
-    echo "    global _start" >> "learn-$((i+1)).s"
-    echo "_start:" >> "learn-$((i+1)).s"
+    mkdir -p "$((i+1))-${cats[$i]}" 
+    touch "$((i+1))-${cats[$i]}/learn-$((i+1)).s"
+    echo "section .data" > "$((i+1))-${cats[$i]}/learn-$((i+1)).s"
+    echo "section .text" >> "$((i+1))-${cats[$i]}/learn-$((i+1)).s"
+    echo "    global _start" >> "$((i+1))-${cats[$i]}/learn-$((i+1)).s"
+    echo "_start:" >> "$((i+1))-${cats[$i]}/learn-$((i+1)).s"
 done
